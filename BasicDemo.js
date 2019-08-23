2f7f5006e43aa04e4184c0623061645727713065
pritirathi <rathipriti@yahoo.in>
2019-08-23 09:45:10 +0530

    Basic demo

--- BasicDemo.js
+++ BasicDemo.js
@@ -6,3 +6,20 @@ if(true)
 }
 
 console.log(mod)
+
+switch(mod){
+case 10:
+    console.log('Ten')
+    break;
+case 20:
+        console.log('Twenty')
+        break;
+default:
+    console.log('other')
+
+}
+
+for(let x=0;x<=10;x)
+{
+    console.log(x)
+}
